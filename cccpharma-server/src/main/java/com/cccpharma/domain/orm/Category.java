5b79ccc83400ec22da8028a66ad7bb1b2774cbfb
ViniFarias <vinifarias.vf@gmail.com>
2018-11-22 23:10:57 -0300

    Adição das classes ORM

---
+++ cccpharma-server/src/main/java/com/cccpharma/domain/orm/Category.java
@@ -0,0 +1,29 @@
+package com.cccpharma.domain.orm;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    @NonNull
+    private String name;
+
+    @Column
+    @NonNull
+    private Double discount;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
