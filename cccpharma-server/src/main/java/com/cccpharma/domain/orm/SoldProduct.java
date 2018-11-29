03d45d86494ea2ec2b0aaa387e13ae23763ee394
ViniFarias <vinifarias.vf@gmail.com>
2018-11-28 22:02:45 -0300

    Adição da classe SoldProduct

---
+++ cccpharma-server/src/main/java/com/cccpharma/domain/orm/SoldProduct.java
@@ -0,0 +1,31 @@
+package com.cccpharma.domain.orm;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SoldProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private Integer productsQuantity;
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id")
+    @NotNull
+    private Sale sale;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    @NotNull
+    private Product product;
+}
