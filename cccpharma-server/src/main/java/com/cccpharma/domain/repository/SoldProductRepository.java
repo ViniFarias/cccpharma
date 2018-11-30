2d53caf2c76d6b993f8d6834608c1df194aeba46
ViniFarias <vinifarias.vf@gmail.com>
2018-11-29 21:53:54 -0300

    Adição do repositório do produto vendido

--- cccpharma-server/src/main/java/com/cccpharma/domain/repository/SoldProductRepository.java
+++ cccpharma-server/src/main/java/com/cccpharma/domain/repository/SoldProductRepository.java
@@ -1,9 +1,8 @@
 package com.cccpharma.domain.repository;
 
-import com.cccpharma.domain.orm.Item;
 import com.cccpharma.domain.orm.SoldProduct;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemRepository extends CrudRepository<SoldProduct, Long> {}
+public interface SoldProductRepository extends CrudRepository<SoldProduct, Long> {}
