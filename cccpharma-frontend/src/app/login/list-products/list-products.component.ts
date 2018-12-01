6e10086564de710d17017ad03a7164d1033cb7a9
Axel <axel.cris2011@hotmail.com>
2018-11-30 22:48:46 -0300

    Adicionado icones interativos na seleção de produtos

--- cccpharma-frontend/src/app/login/list-products/list-products.component.ts
+++ cccpharma-frontend/src/app/login/list-products/list-products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductsComponent implements OnInit {
 
-  constructor() { }
+  // Colors
+  foodColor: boolean;
+  comesticColor: boolean;
+  medicinesColor: boolean;
+  personalColor: boolean;
+  selectAllColor: boolean;
 
-  ngOnInit() {
+
+  constructor() { 
+    this.toAllColor(false);
   }
 
+  ngOnInit() {
+    this.toAllColor(false);
+  }
+
+  color(name: string) {
+    if (name == 'selectAll') {
+      const color = !this.selectAllColor;
+      this.toAllColor(color);
+    } else {
+      this[name] = !this[name];
+    }
+  }
+
+  selectOption(name: string) {
+    this.color(name);
+  }
+
+  toAllColor(op: boolean) {
+    this.foodColor = op;
+    this.comesticColor = op;
+    this.medicinesColor = op;
+    this.personalColor = op;
+    this.selectAllColor = op;
+  }
+  
 }
